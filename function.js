6c6b7a6b8a02484c4903c4a097e28750c1cf9ab6
skghosh87 <sarghosh@gmail.com>
2025-08-12 12:37:25 +0600

    Conceptional Session

---
+++ function.js
@@ -0,0 +1,16 @@
+/**1st Program  */
+// function ATM() {
+//   let amount = 4000;
+//   console.log("Welcome to PH Bank Ltd");
+//   console.log("Your Amount " + amount + " Taka");
+// }
+// ATM();
+
+/**2nd Program Practice Square */
+function square(input) {
+  let squareNum = input * input;
+  //   console.log(squareNum);
+  return squareNum;
+}
+let result = square(49);
+console.log(result);
