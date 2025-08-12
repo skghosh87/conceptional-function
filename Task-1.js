0ace54e5960b6b91db8682bf6452a4964172c4a1
skghosh87 <sarghosh@gmail.com>
2025-08-12 20:05:56 +0600

    Task1

---
+++ Task-1.js
@@ -0,0 +1,17 @@
+function sum(input1, input2) {
+  let result = input1 + input2;
+  // console.log(result);
+  return result;
+}
+function multiply(input1, input2) {
+  let result = input1 * input2;
+  // console.log(result);
+  return result;
+}
+// let result = sum(10, 20);
+// let result = multiply(10, 20);
+
+let a = 4;
+let b = 5;
+let result = sum(a, b) + multiply(a, b);
+console.log(result);
